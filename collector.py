2e63ef04f3a0127b562ac77f83b34c494f8afd39
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-11-03 22:48:55 +0100

    Update

--- collector.py
+++ collector.py
@@ -9,3 +9,13 @@ needed to collect your data from the html page
 import utils
 import collector_utils
 
+n_file = input("number of file (1, 2, 3): ")
+n_idx = input("start idx: ")
+wiki_link_df = collector_utils.data_html_to_dataframe('data/movies' + n_file + '.html')
+for i in range(int(n_idx)-1, len(wiki_link_df)):
+    try:
+        collector_utils.save_webpage(wiki_link_df.iloc[i]['links'],
+                                     'article_' + wiki_link_df.iloc[i]['idx'] + '.html')
+    except:
+        print(wiki_link_df.iloc[i]['idx'])
+
--- collector_utils.py
+++ collector_utils.py
@@ -5,3 +5,30 @@
 python file that stores the function
 used in collector.py
 """
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup as bs
+import pandas as pd
+
+
+def save_webpage(url, file_name):
+    html = urlopen(url)
+    page_content = html.read()
+    with open('Wikipedia/' + file_name, 'wb') as fid:
+        fid.write(page_content)
+
+
+def data_html_to_dataframe(file_name):
+    soup = bs(open(file_name), "html.parser")
+    wiki_links = {'idx': [],
+                  'links': []}
+
+    for tr in soup.findAll("tr"):
+        for td in tr.findAll("td"):
+            if 'http' in td.text:
+                wiki_links['links'].append(td.text)
+            else:
+                wiki_links['idx'].append(td.text)
+    df = pd.DataFrame(wiki_links, columns=['idx', 'links'])
+
+    return df

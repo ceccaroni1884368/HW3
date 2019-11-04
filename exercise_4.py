d351e8186f3af776c893f1919d1d1123a09e7667
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-11-04 22:56:15 +0100

    Update exercise_4.py

--- exercise_4.py
+++ exercise_4.py
@@ -5,3 +5,24 @@
 python file that contains the implementation
 of the algorithm that solves problem 4.
 """
+
+
+def algorithm_palindrome_sub_string_len(string, idx_start, idx_end):
+    if idx_end == idx_start:
+        return 1
+    if string[idx_start] == string[idx_end]:
+        return algorithm_palindrome_sub_string_len(string, idx_start+1, idx_end-1) + 2
+    right = algorithm_palindrome_sub_string_len(string, idx_start+1, idx_end)
+    left = algorithm_palindrome_sub_string_len(string, idx_start, idx_end-1)
+
+    if right > left:
+        return right
+    else:
+        return left
+
+
+def palindrome_sub_string_len(string):
+    return algorithm_palindrome_sub_string_len(string, 0, len(string)-1)
+
+
+print(palindrome_sub_string_len(input('Insert string: ')))

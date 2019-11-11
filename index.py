f31b18a87d1df03dcce773bedb9cdfb930c83139
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-11-12 00:50:16 +0100

    Update index.py

--- index.py
+++ index.py
@@ -5,16 +5,34 @@
 python file that once executed generate
 the indexes of the Search engines
 """
+import json
 import utils
 import index_utils
 
-
+# Dataframe
 dataframe = utils.load_data(utils.list_links_file_in_directory_by_extension('Wikipedia/', '.tsv')[0])
 
-documents = []
-for i in range(len(dataframe[['Intro']])):
-    documents.append(index_utils.format_document(str(dataframe['Intro'].iloc[i]) + str(dataframe['Plot'].iloc[i])))
 
-index_utils.save_inverted_index(documents)
+def generate_inverted_index():
+    documents = []
+    for i in range(len(dataframe[['Intro']])):
+        documents.append(index_utils.format_document(str(dataframe['Intro'].iloc[i]) + str(dataframe['Plot'].iloc[i])))
 
-#print(index_utils.cosine_similar('Disney', dataframe.fillna('None'), 'Plot', 20))
+    index_utils.save_inverted_index(documents)
+
+
+def conjunctive_query():
+    query = input("Search: ")
+    query = index_utils.format_document(query).split(" ")
+
+    with open('inverted_index.json') as json_file:
+        inverted_index = json.load(json_file)
+
+    idx = set(inverted_index[query[0]])
+    for word in query:
+        idx = idx.intersection(inverted_index[word])
+    idx = list(idx)
+
+    return dataframe[['Title', 'Intro', 'Wikipedia Url']].iloc[idx]
+
+print(conjunctive_query())

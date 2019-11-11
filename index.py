e0182d47bf2d46633661e6666115d0ee16933918
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-11-11 10:53:13 +0100

    Update index.py

--- index.py
+++ index.py
@@ -11,4 +11,10 @@ import index_utils
 
 dataframe = utils.load_data(utils.list_links_file_in_directory_by_extension('Wikipedia/', '.tsv')[0])
 
-print(index_utils.cosine_similar('Doctors', dataframe.fillna('None'), 'Title', 3))
+documents = []
+for i in range(len(dataframe[['Intro']])):
+    documents.append(str(dataframe['Intro'].iloc[i]) + str(dataframe['Plot'].iloc[i]))
+
+index_utils.save_inverted_index(documents)
+
+#print(index_utils.cosine_similar('Doctors', dataframe.fillna('None'), 'Title', 3))

b4d60f4988731f6a8086e4942f66f0f2b0b1949b
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-11-12 12:16:18 +0100

    Update collector.py

--- collector.py
+++ collector.py
@@ -6,7 +6,6 @@ python file that contains the line of code
 needed to collect your data from the html page
 (from which you get the urls) and Wikipedia
 """
-import utils
 import collector_utils
 
 n_file = input("Number of file (1, 2, 3): ")

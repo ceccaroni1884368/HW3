e90ea276334b58e46594f8cde4d811d993865886
ceccaroni1884368 <56381002+ceccaroni1884368@users.noreply.github.com>
2019-11-12 12:16:45 +0100

    Update index.py

--- index.py
+++ index.py
@@ -10,29 +10,40 @@ import utils
 import index_utils
 
 # Dataframe
-dataframe = utils.load_data(utils.list_links_file_in_directory_by_extension('Wikipedia/', '.tsv')[0])
+dataframe = utils.load_dataframe()
 
 
 def generate_inverted_index():
+    # Save Format Documents (Intro + Plot)
     documents = []
     for i in range(len(dataframe[['Intro']])):
-        documents.append(index_utils.format_document(str(dataframe['Intro'].iloc[i]) + str(dataframe['Plot'].iloc[i])))
+        documents.append(str(dataframe['Intro'].iloc[i]) + str(dataframe['Plot'].iloc[i]))
+    index_utils.save_format_documents(documents, 'format_documents')
 
+    # Save Inverted Index
+    with open('Json/format_documents.json') as json_file:
+        documents = json.load(json_file)
     index_utils.save_inverted_index(documents)
 
 
-def conjunctive_query():
-    query = input("Search: ")
-    query = index_utils.format_document(query).split(" ")
+def generate_inverted_index_score():
+    with open('Json/format_documents.json') as json_file:
+        documents = json.load(json_file)
 
-    with open('inverted_index.json') as json_file:
+    with open('Json/inverted_index.json') as json_file:
         inverted_index = json.load(json_file)
 
-    idx = set(inverted_index[query[0]])
-    for word in query:
-        idx = idx.intersection(inverted_index[word])
-    idx = list(idx)
+    for word in inverted_index:
+        for i in range(len(inverted_index[word])):
+            inverted_index[word][i] = [inverted_index[word][i],
+                                       index_utils.tfidf(word,
+                                                         documents[inverted_index[word][i]],
+                                                         len(inverted_index[word]),
+                                                         len(documents))]
+    # Save Inverted Index Score
+    with open('Json/inverted_index_score.json', 'w') as f:
+        json.dump(inverted_index, f)
 
-    return dataframe[['Title', 'Intro', 'Wikipedia Url']].iloc[idx]
 
-print(conjunctive_query())
+generate_inverted_index()
+generate_inverted_index_score()
